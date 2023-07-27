fb95eaba15acda3094fecae983605d0c09bda6a6
Dimitar Vasilev <captaingetrekt@gmail.com>
2023-07-27 20:53:00 +0300

    Added and setup unit tests project

---
+++ TravelHub.UnitTests/BookingServiceTests.cs
@@ -0,0 +1,30 @@
+﻿namespace TravelHub.UnitTests
+{
+    using TravelHub.Core.Contracts;
+    using TravelHub.Core.Repositories;
+    using TravelHub.Infrastructure;
+    using Microsoft.EntityFrameworkCore;
+
+    [TestFixture]
+    public class BookingServiceTests
+    {
+        private IRepository repo = null!;
+
+        private IBookingService bookingService = null!;
+
+        private TravelHubContext dbContext;
+
+        [SetUp]
+        public void Setup()
+        {
+            var contextOptions = new DbContextOptionsBuilder<TravelHubContext>()
+                .UseInMemoryDatabase("TravelHub")
+                .Options;
+
+            this.dbContext = new TravelHubContext(contextOptions);
+
+            this.dbContext.Database.EnsureDeletedAsync();
+            this.dbContext.Database.EnsureCreatedAsync();
+        }
+    }
+}
